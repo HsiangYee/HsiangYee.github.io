4be2f83c3786ee0d862fac4805b2b44c38f89ffd
HsiangYee <hsiangyeeht@gmail.com>
2022-12-31 01:11:08 +0800

    update

--- static/js/tmp/layout.js
+++ static/js/tmp/layout.js
@@ -21,67 +21,68 @@ const menuContent = `
         </div>
 
         <div class="sidebar">
-        <nav class="mt-2" id="menu">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <li class="nav-item">
-                    <a href="/views/index.html" title="股市計算機 | 首頁" class="nav-link">
-                        <i class="fas fa-home custom-icon"></i>
-                        <p>首頁</p>
-                    </a>
-                </li>
-                <li class="nav-header">功能</li>
-                <li class="nav-item">
-                    <a href="/views/roi.html" title="股市計算機 | 報酬率快速算" class="nav-link">
-                        <i class="fas fa-calculator custom-icon"></i>
-                        <p>報酬率快速算 <span class="text-danger"><small></small></span></p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/views/balance.html" title="股市計算機 | 損益平衡試算" class="nav-link">
-                        <i class="fas fa-calculator custom-icon"></i>
-                        <p>損益平衡試算 <span class="text-danger"><small></small></span></p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/views/dayTrading.html" title="股市計算機 | 現股當沖試算" class="nav-link">
-                        <i class="fas fa-calculator custom-icon"></i>
-                        <p>現股當沖試算</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/views/dayTradingMini.html" title="股市計算機 | 現股當沖試算(Mini)" class="nav-link">
-                        <i class="fas fa-calculator custom-icon"></i>
-                        <p>現股當沖試算(Mini)</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/views/ETF.html" title="股市計算機 | ETF 試算" class="nav-link">
-                        <i class="fas fa-calculator custom-icon"></i>
-                        <p>ETF 試算 <span class="text-danger"><small></small></span></p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/views/warrant.html" title="股市計算機 | 權證試算" class="nav-link">
-                        <i class="fas fa-calculator custom-icon"></i>
-                        <p>權證試算 <span class="text-danger"><small></small></span></p>
-                    </a>
-                </li>
-                <li class="nav-header">網站資訊</li>
-                <li class="nav-item">
-                    <a href="/views/question.html" title="股市計算機 | 常見問題" class="nav-link">
-                    <i class="fas fa-question custom-icon"></i>
-                        <p>常見問題</p>
-                    </a>
-                </li>
-                <!--
-                <li class="nav-item">
-                    <a href="/views/author.html" title="股市計算機 | 關於作者 & 網站" class="nav-link">
-                        <i class="far fa-user-circle custom-icon"></i>
-                        <p>關於作者 & 網站</p>
-                    </a>
-                </li> -->
-            </ul>
-        </nav>
+            <nav class="mt-2" id="menu">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <li class="nav-item">
+                        <a href="/views/index.html" title="股市計算機 | 首頁" class="nav-link">
+                            <i class="fas fa-home custom-icon"></i>
+                            <p>首頁</p>
+                        </a>
+                    </li>
+                    <li class="nav-header">功能</li>
+                    <li class="nav-item">
+                        <a href="/views/roi.html" title="股市計算機 | 報酬率快速算" class="nav-link">
+                            <i class="fas fa-calculator custom-icon"></i>
+                            <p>報酬率快速算 <span class="text-danger"><small></small></span></p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/views/balance.html" title="股市計算機 | 損益平衡試算" class="nav-link">
+                            <i class="fas fa-calculator custom-icon"></i>
+                            <p>損益平衡試算 <span class="text-danger"><small></small></span></p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/views/dayTrading.html" title="股市計算機 | 現股當沖試算" class="nav-link">
+                            <i class="fas fa-calculator custom-icon"></i>
+                            <p>現股當沖試算</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/views/dayTradingMini.html" title="股市計算機 | 現股當沖試算(Mini)" class="nav-link">
+                            <i class="fas fa-calculator custom-icon"></i>
+                            <p>現股當沖試算(Mini)</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/views/ETF.html" title="股市計算機 | ETF 試算" class="nav-link">
+                            <i class="fas fa-calculator custom-icon"></i>
+                            <p>ETF 試算 <span class="text-danger"><small></small></span></p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/views/warrant.html" title="股市計算機 | 權證試算" class="nav-link">
+                            <i class="fas fa-calculator custom-icon"></i>
+                            <p>權證試算 <span class="text-danger"><small></small></span></p>
+                        </a>
+                    </li>
+                    <li class="nav-header">網站資訊</li>
+                    <li class="nav-item">
+                        <a href="/views/question.html" title="股市計算機 | 常見問題" class="nav-link">
+                        <i class="fas fa-question custom-icon"></i>
+                            <p>常見問題</p>
+                        </a>
+                    </li>
+                    <!--
+                    <li class="nav-item">
+                        <a href="/views/author.html" title="股市計算機 | 關於作者 & 網站" class="nav-link">
+                            <i class="far fa-user-circle custom-icon"></i>
+                            <p>關於作者 & 網站</p>
+                        </a>
+                    </li> -->
+                </ul>
+            </nav>
+        </div>
 
         <!-- navbar -->
         <ins class="adsbygoogle"
@@ -143,18 +144,7 @@ $.confirm({
     title: '',
     type: 'blue',
     columnClass: 'col-12 col-lg-4 col-md-6 p-3 overflow-scroll',
-    content: ADcontent[ADnumber]['content'] + `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8969180932862200"
-    crossorigin="anonymous"></script>
-<!-- show -->
-<ins class="adsbygoogle"
-    style="display:block"
-    data-ad-client="ca-pub-8969180932862200"
-    data-ad-slot="3744963457"
-    data-ad-format="auto"
-    data-full-width-responsive="true"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>`,
+    content: ADcontent[ADnumber]['content'],
     buttons: {
         cancel: {
             text: '沒 興 趣',
